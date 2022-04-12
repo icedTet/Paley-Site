83d27c53174ed0e63e16e225e92e70b447034a15
root <dragonpause@gmail.com>
2022-04-12 16:07:53 +0000

    navbar start

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+    return <div className="w-full h-16 flex flex-row justify-center">
+        <div className="flex flex-row">
+            <img src='https://cdn.discordapp.com/attachments/879584371881947166/963220654826586132/PaleyButBetter.png' className='w-auto h-16'></img>
+            <span>If I Were King</span>
+        </div>
+
+        <div>
+            weehhwjehjwhekjhwkjhwef
+        </div>
+        
+
+    </div>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link'
+import React from 'react'
 import Layout from '../components/Layout'
+import { Navbar } from '../components/Navbar'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
+    <Navbar />
     <h1 className={"text-blue-500"}>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
